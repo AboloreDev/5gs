07fe679c900c23779b78599b1a7b50d884184255
Alabi Fathiu <alabiabolore4@gmail.com>
2025-01-17 22:32:26 +0000

    Initial Commit

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -6,7 +6,7 @@ export default function DashboardLayout({ children }) {
   return (
     <div className="h-screen bg-black flex">
       {/* Left */}
-      <div className="w-[10%] border-r-2 md:w-[8%] lg:w-[20%] xl:w-[14%] flex flex-col gap-14 fixed h-screen">
+      <div className="w-[10%] border-r-2 md:w-[8%] lg:w-[20%] xl:w-[14%] flex flex-col gap-14 sticky h-screen">
         {/* Logo */}
         <div className="w-60px md:w-100px p-2 flex justify-start md:justify-center">
           <Image src="/logo.png" alt="Logo" width={100} height={100} />
@@ -14,7 +14,7 @@ export default function DashboardLayout({ children }) {
         <Sidebar />
       </div>
       {/* right */}
-      <div className="w-screen md:w-[92%] lg:w-[80%] xl:w-[86%] overflow-scroll">
+      <div className="w-[90%] md:w-[92%] lg:w-[80%] xl:w-[86%] overflow-scroll">
         <ClientProvider>{children}</ClientProvider>
       </div>
     </div>
